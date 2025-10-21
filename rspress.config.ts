653b18ee0ebf796a1af32af1695c825702f0ea8a
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2025-10-21 18:21:15 -0300

    Rspress + Theme

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,19 +2,20 @@ import * as path from 'node:path';
 import { defineConfig } from '@rspress/core';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'My Site',
-  icon: '/rspress-icon.png',
+  root: path.join(__dirname, 'content'),
+  title: 'Kitledger Docs',
+  icon: './content/public/brand/icon.png',
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: './brand/logo.png',
+    dark: './brand/logo-wt.png',
   },
+  globalStyles: path.join(__dirname, 'index.css'),
   themeConfig: {
     socialLinks: [
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/web-infra-dev/rspress',
+        content: 'https://github.com/kitledger',
       },
     ],
   },
